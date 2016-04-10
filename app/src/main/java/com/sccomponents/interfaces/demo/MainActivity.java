a79708ea9756528c963b7cf761ebd2fa81bddedf
Sam <samuele.carassai@gmail.com>
2016-04-10 23:53:09 +0700

    Stable release

---
+++ app/src/main/java/com/sccomponents/interfaces/demo/MainActivity.java
@@ -0,0 +1,15 @@
+package com.sccomponents.interfaces.demo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // Load
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
